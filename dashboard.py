ca22ed872813659a5daca277b30186ad986d35f0
mitraddgr <116553475+mitraddgr@users.noreply.github.com>
2022-11-28 12:48:49 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -275,7 +275,8 @@ def st_shap(plot, height=None):
 def get_proba_for_client(client_id:str):
     url = f'https://ocr-p7-api.herokuapp.com//predict?id_client={client_id}'
     x = requests.post(url)
-    return x.json()['probability']
+    return x.json()[1]
+   # return x.json()['probability']
 
 #def get_proba_for_client(client_id:int):
 #    url = f'https://ocr-p7-api.herokuapp.com//predict?id_client={client_id}'
